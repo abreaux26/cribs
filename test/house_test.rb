ad8653012dccd701c4ba30cd19f5357d04080a8d
Angel Breaux <abreaux@detaso.com>
2020-12-09 14:43:46 -0600

    Add test for area of the house

--- test/house_test.rb
+++ test/house_test.rb
@@ -42,4 +42,38 @@ class HouseTest < Minitest::Test
 
     assert_equal true, house.above_market_average?
   end
+
+  def test_rooms_from_category
+    house = House.new("$600000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_3], house.rooms_from_category(:living_room)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_area
+    house = House.new("$600000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
 end
